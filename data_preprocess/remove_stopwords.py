4a8046becbd954686e4470ea14caecfe542619bb
qinghaopeng <qinghaopeng0730@gmail.com>
2020-04-04 14:28:08 -0700

    stop words

---
+++ data_preprocess/remove_stopwords.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import json
+
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+def remove_stopwords(input_path, output_path):
+    with open(input_path, 'r') as input_file:
+        with open(output_path, 'w') as output_file:
+            for line in input_file:
+                content = json.loads(line)
+                tokens = content['doc_token']
+                content['doc_token'] = [token for token in tokens if token not in stop_words]
+                output_file.write(json.dumps(content))
+                output_file.write('\n')
+    
+if __name__ == '__main__':
+
+#    input_directory = sys.argv[1]
+#    output_directory = sys.argv[2]
+#    if not os.path.exists(output_directory):
+#        os.makedirs(output_directory)
+#    
+#    for filename in os.listdir(input_directory):
+#        if filename.endswith(".json"):
+#            input_path = os.path.join(input_directory, filename)
+#            output_path = os.path.join(output_directory, filename)
+#            remove_stopwords(input_path, output_path)
+#    
+    remove_stopwords("../../test.json", "../../test_nonstop.json")
