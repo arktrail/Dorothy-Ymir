350e339b19f19748fb197c25ac2f2a6bd398ebba
clevercurlcurl <1608815224@qq.com>
2020-05-24 14:30:54 -0400

    change example

--- model/HFT-CNN/example.sh
+++ model/HFT-CNN/example.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-DataDIR=./My_data
+DataDIR=./summary_only_nonstop
 Train=${DataDIR}/train.json
 Test=${DataDIR}/test.json
 Valid=${DataDIR}/valid.json
@@ -9,14 +9,24 @@ EmbeddingWeightsPath=./Word_embedding/
 ## Network Type (XML-CNN,  CNN-Flat,  CNN-Hierarchy,  CNN-fine-tuning or Pre-process)
 ModelType=Pre-process
 ### the limit of the sequence 
-USE_WORDS=150
+USE_WORDS=300
 ### Tree file path
-TreefilePath=./Tree/CPC_new.tree
+TreefilePath=./Tree/CPC_subclass.tree
+### Data info
+LabelName=doc_label
+InputTextName=doc_token
 
-mkdir -p CNN
-mkdir -p CNN/PARAMS
-mkdir -p CNN/LOG
-mkdir -p CNN/RESULT
+### Don`t change the OutputDIR folder here!
+OutputDIR=CNN 
+mkdir -p ${OutputDIR}
+mkdir -p ${OutputDIR}/PARAMS
+mkdir -p ${OutputDIR}/LOG
+mkdir -p ${OutputDIR}/RESULT
 mkdir -p Word_embedding
 
-python3.5 ./train.py ${Train} ${Test} ${Valid} ${EmbeddingWeightsPath} ${ModelType} ${TreefilePath} ${USE_WORDS}
+### Preprocess 
+python3.5 train.py ${Train} ${Test} ${Valid} ${EmbeddingWeightsPath} ${ModelType} ${TreefilePath} ${USE_WORDS} ${LabelName} ${InputTextName}
+### Training model
+## Network Type (XML-CNN,  CNN-Flat,  CNN-Hierarchy,  CNN-fine-tuning or Pre-process)
+ModelType=CNN-Hierarchy
+python3.5 train.py ${Train} ${Test} ${Valid} ${EmbeddingWeightsPath} ${ModelType} ${TreefilePath} ${USE_WORDS} ${LabelName} ${InputTextName}
