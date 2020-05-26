cbacf193b4bcc411fe4e4ec53eacb9c4a55e5294
qinghaopeng <yyn1228@br005.pvt.bridges.psc.edu>
2020-05-26 10:39:22 -0400

    change file names

---
+++ model/HFT-CNN/train.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+DataDIR=../../../data/summary_only_nonstop
+Train=${DataDIR}/train.json
+Test=${DataDIR}/test.json
+Valid=${DataDIR}/valid.json
+
+## Embedding Weights Type (fastText .bin)
+EmbeddingWeightsPath=./Word_embedding/
+## Network Type (XML-CNN,  CNN-Flat,  CNN-Hierarchy,  CNN-fine-tuning or Pre-process)
+ModelType=Pre-process
+### the limit of the sequence 
+USE_WORDS=300
+### Tree file path
+TreefilePath=./Tree/CPC_subclass.tree
+### Data info
+LabelName=doc_label
+InputTextName=doc_token
+
+### Don`t change the OutputDIR folder here!
+OutputDIR=CNN 
+mkdir -p ${OutputDIR}
+mkdir -p ${OutputDIR}/PARAMS
+mkdir -p ${OutputDIR}/LOG
+mkdir -p ${OutputDIR}/RESULT
+mkdir -p Word_embedding
+
+### Preprocess 
+python train.py ${Train} ${Test} ${Valid} ${EmbeddingWeightsPath} ${ModelType} ${TreefilePath} ${USE_WORDS} ${LabelName} ${InputTextName}
+### Training model
+## Network Type (XML-CNN,  CNN-Flat,  CNN-Hierarchy,  CNN-fine-tuning or Pre-process)
+ModelType=CNN-Hierarchy
+python train.py ${Train} ${Test} ${Valid} ${EmbeddingWeightsPath} ${ModelType} ${TreefilePath} ${USE_WORDS} ${LabelName} ${InputTextName}
